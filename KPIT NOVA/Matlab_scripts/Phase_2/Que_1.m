1158587faf1a4c5c94b4495d5a4681bbb56f116e
KJ-Sagar <kjagannathsagar@gmail.com>
2025-02-16 22:21:46 +0530

    Update Que_1.m

--- KPIT NOVA/Matlab_scripts/Phase_2/Que_1.m
+++ KPIT NOVA/Matlab_scripts/Phase_2/Que_1.m
@@ -1,6 +1,11 @@
 clc;
 clear;
-a = input("enter the array");
-for i = 1:2:length(a)
-    disp(a(i))
+x = input("enter the array ");
+y = [];
+t=1;
+for i = 1:2:length(x)
+    y(t)=x(i);
+    t=t+1;
 end
+disp("The result is ");
+disp(y);
